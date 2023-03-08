2683db66d04fdb3b264b888c38ad87e32f7479e4
k-nakase <b22a0112@tcs-one.com>
2023-03-08 17:26:46 +0900

    react-test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function App() {
   const value = 'Nakase';
-  return <div>Hello {value}</div>;
+  const value1 = 'Kota';
+  return (
+    <div>
+      <h3>Hello {value}</h3>
+      <h3>Hello {value1}</h3>
+    </div>
+  );
 }
 
 export default App;
