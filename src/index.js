ca6f93128f5e6cc90acea24c7aef076b3a784c0b
k-nakase <b22a0112@tcs-one.com>
2023-03-10 17:34:07 +0900

    web

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Top from './Top';
-import Table from './Table';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//ReactDOM.render(<Top />, document.getElementById('root'));
-//ReactDOM.render(<Table />, document.getElementById('root'));
